897299cfdb149488ac66b91e8d2e17bed930994b
Pierre-Olivier Latour <pol@mac.com>
2014-10-14 15:20:52 -0700

    Simplified CLT

--- CLT/main.m
+++ CLT/main.m
@@ -1,69 +1,27 @@
 #import "XLFacilityMacros.h"
-#import "XLFileLogger.h"
-#import "XLDatabaseLogger.h"
-#import "XLCallbackLogger.h"
-#import "XLASLLogger.h"
 #import "XLTelnetServerLogger.h"
 #import "XLHTTPServerLogger.h"
 
+static int _counter = 0;
+
+static void _RunLoopTimerCallBack(CFRunLoopTimerRef timer, void* info) {
+  if (_counter % 2 == 0) {
+    XLOG_VERBOSE(@"Tick");
+  } else {
+    XLOG_VERBOSE(@"Tock");
+  }
+  _counter += 1;
+}
+
 int main(int argc, const char* argv[]) {
   @autoreleasepool {
-    [XLSharedFacility setLogsUncaughtExceptions:YES];
-    [XLSharedFacility setLogsInitializedExceptions:YES];
-    [XLSharedFacility setCapturesStandardError:YES];
+    [XLSharedFacility addLogger:[[XLTelnetServerLogger alloc] init]];
+    [XLSharedFacility addLogger:[[XLHTTPServerLogger alloc] init]];
     
-    [[XLFacility sharedFacility] addLogger:[[XLFileLogger alloc] initWithFilePath:@"temp.log" append:NO]];
-    XLDatabaseLogger* databaseLogger = [[XLDatabaseLogger alloc] initWithDatabasePath:@"temp.db" appVersion:1];
-    databaseLogger.logRecordFilter = ^BOOL(XLLogger* logger, XLLogRecord* record) {
-      return record.logLevel >= kXLLogLevel_Error;
-    };
-    [[XLFacility sharedFacility] addLogger:databaseLogger];
-    [[XLFacility sharedFacility] addLogger:[XLCallbackLogger loggerWithCallback:^(XLCallbackLogger* logger, XLLogRecord* record) {
-      fprintf(stdout, "-> %s\n", XLConvertNSStringToUTF8CString(record.message));
-      fflush(stdout);
-    }]];
-    [[XLFacility sharedFacility] addLogger:[XLASLLogger sharedLogger]];
-    [[XLFacility sharedFacility] addLogger:[[XLTelnetServerLogger alloc] init]];
-    [[XLFacility sharedFacility] addLogger:[[XLHTTPServerLogger alloc] init]];
+    CFRunLoopTimerRef timer = CFRunLoopTimerCreate(kCFAllocatorDefault, 0.0, 1.0, 0, 0, _RunLoopTimerCallBack, NULL);
+    CFRunLoopAddTimer(CFRunLoopGetMain(), timer, kCFRunLoopCommonModes);
     
-    fprintf(stdout, "Hello World!\nAll is fine!\n");
-    NSLog(@"We have started!");
-    
-//    XLOG_ABORT(@"HEY");
-    
-    XLOG_DEBUG(@"Hello %@", @"World!");
-    XLOG_VERBOSE(@"Hello %@", @"World!");
-    CFRunLoopRunInMode(kCFRunLoopDefaultMode, 0.1, false);
-    XLOG_INFO(@"Hello %@", @"World!");
-    XLOG_WARNING(@"Hello %@", @"World!");
-    XLOG_ERROR(@"Hello %@", @"World!");
-    
-    @try {
-      [[NSArray array] objectAtIndex:1];
-    }
-    @catch (NSException* exception) {
-#pragma unused(exception)
-//      XLOG_EXCEPTION(exception);
-//      [exception raise];
-    }
-    
-    [databaseLogger purgeRecordsBeforeAbsoluteTime:(CFAbsoluteTimeGetCurrent() - 10.0)];
-    [databaseLogger enumerateRecordsAfterAbsoluteTime:0.0 backward:YES maxRecords:2 usingBlock:^(int appVersion, XLLogRecord* record, BOOL* stop) {
-      NSLog(@"%i = %@", (int)appVersion, record.message);
-    }];
-    
-    for (int i = 1; i <= 5; ++i) {
-      [XLSharedFacility logMessageWithLevel:(kXLLogLevel_Verbose + i % 4) format:@"PING %i", (int)i];
-      CFRunLoopRunInMode(kCFRunLoopDefaultMode, 1.0, false);
-    }
-    
-    XLOG_INFO(@"Waiting 3 seconds...");
-    CFRunLoopRunInMode(kCFRunLoopDefaultMode, 3.0, false);
-    
-    for (int i = 6; i <= 10; ++i) {
-      [XLSharedFacility logMessageWithLevel:(kXLLogLevel_Verbose + i % 4) format:@"PING %i", (int)i];
-      CFRunLoopRunInMode(kCFRunLoopDefaultMode, 1.0, false);
-    }
+    CFRunLoopRun();
   }
   return 0;
 }
